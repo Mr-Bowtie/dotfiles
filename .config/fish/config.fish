ceadb4f82d7af822ed88000c3e5f8abe51afbc26
Ian Withers <ian.neugent-withers@eatstreet.com>
2023-11-27 13:55:50 -0500

    config: mac fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,13 +1,81 @@
+fish_vi_key_bindings
 
-
-export PATH=/home/mr_bowtie/.rbenv/shims:/home/mr_bowtie/.local/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/lib/jvm/default/bin:/var/lib/snapd/snap/bin:/usr/local/go/bin:/usr/local/go/bin:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl:/home/mr_bowtie/.gem/ruby/2.7.0/bin:/home/mr_bowtie/Documents/shell_scripts:/home/mr_bowtie/.luaver:/home/mr_bowtie/.rbenv:/home/mr_bowtie/scripts:/home/mr_bowtie/.local/share/nvm
-export FLYCTL_INSTALL="/home/mr_bowtie/.fly"
-export PATH="$FLYCTL_INSTALL/bin:$PATH"
+# export PATH="$PATH:$HOME/.rbenv/shims/"
 export VISUAL=nvim
 export EDITOR="$VISUAL"
-export BROWSER=firefox
-status --is-interactive; and rbenv init - fish | source
-fish_vi_key_bindings
+export SDKMAN_DIR="$HOME/.sdkman"
+# export GPG_TTY=$(tty)
+export NVM_DIR="$HOME/.nvm"
+export GOBIN="$GOPATH/bin"
+
+# ====== old ZSH configs that do not currently work ============
+# [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
+# export PATH=$PATH:~/go/bin
+# export PATH="$PATH:/usr/local/bin/elixir"
+
+# Set PATH, MANPATH, etc..., for Homebrew
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+
+#bobthefish options (unused currently)
+# set -g theme_display_ruby yes 
+# set -g theme_display_vi yes
+# set -g theme_date_format "+%H:%M"
+# set -g theme_color_scheme solarized
+# set -g theme_nerd_fonts yes
+# set -g theme_display_ruby yes
+# set -g theme_title_display_path yes
+# set -g theme_title_use_abbreviated_path no
+
+set -g __fish_sdkman_init "/Users/iann/.sdkman/bin/sdkman-init.sh"
+
+# ===== Aliases ===============================================
+alias ll="exa -lah --icons --color=always --sort=type"
+alias lt="exa -lahT --icons --color=always"
+alias start-driver-staging="FONTAWESOME_NPM_AUTH_TOKEN=B3DC40D3-7291-43D9-8F83-4FF4AF389EC0 npx ionic serve --configuration staging"
+alias ssh-addall="ssh-add --apple-use-keychain ~/.ssh/id_ed25519 ~/.ssh/id_ed25519_personal"
+alias create-secrets='mkdir secrets; cp $HOME/.config/fish/secrets/GITHUB_USER secrets; cp $HOME/.config/fish/secrets/GITHUB_TOKEN secrets'
+alias update-gh-token='rm secrets/GITHUB_TOKEN && touch secrets/GITHUB_TOKEN && echo "$GITHUB_PACKAGES_PAT" >> secrets/GITHUB_TOKEN'
+# get on the box
+alias gotb="aws sso --profile es login && aws ssm start-session --profile=es --target"
+alias re-zup="RAILS_ENV=development docker compose down && RAILS_ENV=development docker compose up"
+alias zup="RAILS_ENV=development RACK_ENV=development docker compose up"
+alias ztup-guard="docker compose -f docker-compose-test.yml up"
+alias ztup-system="SYSTEM_TESTING=true docker compose -f docker-compose-test.yml up"
+alias ztup="docker compose -f docker-compose-test.yml up"
+alias zbuild="RAILS_ENV=development RACK_ENV=development docker compose up --build"
+alias ztbuild="docker compose -f docker-compose-test.yml build"
+alias zdown="docker compose down && docker compose -f docker-compose-test.yml down"
+alias zrun-test="docker exec -it delivery-admin-test bundle exec spring rspec"
+alias zextract="docker cp delivery-admin-test:app/tmp/screenshots ~/Pictures"
+alias solpath="GEM_HOME='$HOME/.local/share/nvim/lsp_servers/solargraph' GEM_PATH='$HOME/.local/share/nvim/lsp_servers/solargraph' "
+alias gtrun="docker compose -f docker-compose.test.yml up --build --abort-on-container-exit --exit-code-from test"
+alias gbuild="docker compose -f docker-compose.local.yml up --build"
+alias gup="docker compose -f docker-compose.local.yml up"
+alias gdeploy="cdk deploy -c app=Geodata -c release=staging --profile-es"
+alias config="git --git-dir=$HOME/.cfg --work-tree=$HOME"
+alias lazy-config="lazygit --git-dir=$HOME/.cfg/ --work-tree=$HOME"
+alias rbenv-doctor="curl -fsSL https://github.com/rbenv/rbenv-installer/raw/main/bin/rbenv-doctor | bash"
+alias docker_clean_images='docker rmi $(docker images -a --filter=dangling=true -q)'
+alias docker_clean_ps='docker rm $(docker ps --filter=status=exited --filter=status=created -q)'
+alias docker_remove_all_images='docker rmi $(docker images -a -q)'
+alias serve-geteatstreet='ng serve --ssl true --ssl-key ~/Programming/eatstreet/eatstreet/master/web/local.eatstreet.com-key.pem --ssl-cert ~/Programming/eatstreet/eatstreet/master/web/local.eatstreet.com.pem'
+alias commits_list='cd ~/Programming/eatstreet/eatstreet/master/web/tools/ && COMMIT_TO_S3=true python3 load-deploy-commits.py deploy-2023-09-15 deploy-2023-09-08'
+alias td_proj="todo.sh projectview"
+alias td="todo.sh"
+alias td_na="todo.sh note add"
+alias td_ne="todo.sh note edit"
+alias td_ns="todo.sh note show"
+alias dev-config="git --git-dir=$HOME/.cfg_new --work-tree=$HOME"
+alias dev-lazy-config="lazygit --git-dir=$HOME/.cfg_new/ --work-tree=$HOME"
+alias fish_config="nvim ~/.config/fish/config.fish"
+
+function es_run_dev
+    cd ~/Programming/eatstreet/eatstreet/master/web/WebContent/redesign/
+    nvm install 'v14.21.3'
+    nvm use
+    npm run dev:watch
+end
 
 function sudo --description "Replacement for Bash 'sudo !!' command to run last command using sudo."
     if test "$argv" = !!
@@ -17,84 +85,43 @@ function sudo --description "Replacement for Bash 'sudo !!' command to run last 
     end
 end
 
-# installing from official repos 
-function ofinstall --description "Install from offical repos and save updated aura package list"
-    sudo aura -Sy $argv
-    sudo aura -B
-    sudo aura -Bc 5
-    cp -r /var/cache/aura/states/ ~/.config/aura/
-    cd ~/.config/aura/
-    git add .
-    git commit -m 'update package list'
-    git push
-    cd ~
+# Setting PATH for Python 3.11
+# The original version is saved in /Users/iann/.config/fish/config.fish.pysave
+set -x PATH "/Library/Frameworks/Python.framework/Versions/3.11/bin" "$PATH"
+status --is-interactive; and rbenv init - fish | source
+pyenv init - | source
+
+thefuck --alias | source
+
+function testing
+    if bw unlock
+        echo your in
+        echo "you're out"
+    end
 end
 
-function aurinstall --description "Install from AUR and save updated aura package list"
-    sudo aura -Ac $argv
-    sudo aura -B
-    sudo aura -Bc 5
-    cp -r /var/cache/aura/states/ ~/.config/aura/
-    cd ~/.config/aura/
-    git add .
-    git commit -m 'update package list'
-    git push
-    cd ~
+function es_sass
+    if pwd | grep web/WebContent/redesign
+        sass app.sass --style=compressed >css/app.css
+        nvm use
+        npm run dev
+    else
+        echo 'please navigate to web/WebContent/redesign in the ES monolith'
+    end
 end
 
-function gac --description "git add all files and commit with provided message"
-    git add -A
-    git commit -m $argv
+function es_sass_mobile
+    if pwd | grep web/WebContent/redesign
+        sass app-mobile.sass --style=compressed >css/app-mobile.css
+        nvm use
+        npm run dev
+    else
+        echo 'please navigate to web/WebContent/redesign in the ES monolith'
+    end
 end
 
-function restart_echo_dev --description "rebuild and run an interactive docker container to see fresh changes for Echo Chamber testing"
-  docker build ./ -t 'echo_chamber:testing'
-  docker run -it -e APP_ENV=development echo_chamber:testing
-end
+function tdls
+  todo.sh ls $argv | sort -n
+end 
 
-#bobthefish options 
-set -g theme_display_ruby yes
-set -g theme_display_vi yes
-set -g theme_date_format "+%H:%M"
-set -g theme_color_scheme solarized
-set -g theme_nerd_fonts yes
-set -g theme_display_ruby yes
-set -g theme_title_display_path yes
-set -g theme_title_use_abbreviated_path no
 
-set LS_COLORS "di=01;33"
-export LS_COLORS
-
-#runs transset-df to make terminal windows transparent when composite manager (xcompmgr) is running : default 75% (which is perfect)
-#transset-df --id "$WINDOWID" >/dev/null
-
-alias ll "exa -lahH --icons --git"
-alias lt "exa -lahHT --icons --git"
-alias grab "scrot '%F_%H-%M.png' -s -e 'mv \$f /home/mr_bowtie/pictures/screenshots/'"
-alias nmlist "nmcli device wifi list"
-alias nmconnect-house "nmcli device wifi connect E0:22:04:70:85:66 password BENDresterACEpre"
-alias nmconnect-ext "nmcli device wifi connect 90:9A:4A:A1:64:E1 password BENDresterACEpre"
-alias qinit "git init && git add . && git commit -m 'inital commit'" #only works if a file has been created in the directory
-alias startup "ruby ~/documents/shell_scripts/startup_script.rb"
-alias steam-picker " ruby ~/programming/steam-selector/lib/main.rb"
-alias ofupdate "sudo aura -Syyu"
-alias aurupdate "sudo aura -Au"
-alias pom "ruby ~/programming/side-projects/pomodoro/main.rb"
-alias wmswap "killall xfwm4 && awesome &"
-alias byobu-rails "BYOBU_WINDOWS=rails-project byobu"
-alias vim nvim
-alias weather "curl wttr.in"
-alias insurgence "wine ~/Downloads/Pokemon\ Insurgence\ 1.2.7\ Core/Game.exe"
-alias config="git --git-dir=$HOME/.cfg --work-tree=$HOME"
-alias lazy-config="lazygit --git-dir=$HOME/.cfg/ --work-tree=$HOME"
-alias rails_env_builder="source /home/mr_bowtie/scripts/new_rails_env"
-alias rdev_up="env RAILS_ENV=development docker compose up"
-alias rtest_up="env RAILS_ENV=test docker compose up"
-alias rdown="docker compose down"
-alias td_proj="todo.sh projectview"
-alias td="todo.sh"
-alias td_na="todo.sh note add"
-alias td_ne="todo.sh note edit"
-alias td_ns="todo.sh note show"
-alias show_breakman_report="firefox --new-window tmp/brakeman.html"
-alias fish_config="nvim ~/.config/fish/config.fish"
