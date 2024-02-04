b77715396ae4b5fb5bd88052323b9234bd3b1fba
Ian Withers <mr.bowtie989@gmail.com>
2024-02-04 15:32:38 -0500

    style: add vimwiki highlight colorl

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,11 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 vim.cmd("highlight Comment guifg=#fa59b2")
+vim.cmd("highlight VimwikiBold guifg=#E0B66B gui=bold")
+vim.cmd("highlight VimwikiCode guifg=#E06B6B")
+vim.cmd("highlight VimwikiHeader1 guifg=#6BA5E0")
+vim.cmd("highlight VimwikiHeader2 guifg=#9d7cd8")
+vim.cmd("highlight VimwikiH1Folding guifg=#5CBF74")
+
 vim.api.nvim_set_hl(0, "@comment", { link = "Comment" })
+
